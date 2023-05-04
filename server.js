808a07ead06254c7ea279a72da4db455a66f00f7
Killian LE VAILLANT <killian.levaillant@gmail.com>
2023-05-04 13:44:51 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Configuring bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Configuring CORS middleware
+app.use(cors());
+
+// Defining the API endpoint
+app.get('/api/data', (req, res) => {
+  const data = {
+    message: 'Hello from the API!'
+  };
+  res.json(data);
+});
+
+// Starting the server
+app.listen(4000, () => {
+  console.log('Server started on port 4000');
+});
